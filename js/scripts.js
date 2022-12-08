1e0348c9bb48bcb40a6fcc2a55b0247a51c56576
Tristan Lewis <94727522+MajestysFiend@users.noreply.github.com>
2022-12-08 00:28:25 -0500

    Added Pokemon list array and removed older content

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,4 @@
-let favoriteFood = 'Brisket';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['grass', 'poison'] },
+    { name: 'Charmander', height: 0.6, weight: 8.5, types: ['fire'] },
+    { name: 'Squirtle', height: 0.5, weight: 9, types: ['water'] }];
