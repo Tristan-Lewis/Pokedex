c5eb62ccd012e849720720e7a15e217c6df16fd1
Tristan Lewis <94727522+MajestysFiend@users.noreply.github.com>
2022-12-10 12:23:16 -0500

    Added object to pokemon list array

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['grass', 'poison'] },
     { name: 'Charmander', height: 0.6, weight: 8.5, types: ['fire'] },
-];
+    { name: 'Squirtle', height: 0.5, weight: 9, types: ['water'] }];
 // Looping through Pokemon list array
 for (i = 0; i < pokemonList.length; i++) {
     // Conditional for adding text to Pokemon height
