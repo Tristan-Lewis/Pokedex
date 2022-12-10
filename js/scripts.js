9407fe21ae545e890bb4558e6e751f9662c57914
Tristan Lewis <94727522+MajestysFiend@users.noreply.github.com>
2022-12-10 12:08:21 -0500

    Added comments, loops,  and output list to web page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
+// Array of Pokemon and their attributes as objects
 let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['grass', 'poison'] },
     { name: 'Charmander', height: 0.6, weight: 8.5, types: ['fire'] },
-    { name: 'Squirtle', height: 0.5, weight: 9, types: ['water'] }];
+];
+// Looping through Pokemon list array
+for (i = 0; i < pokemonList.length; i++) {
+    // Conditional for adding text to Pokemon height
+    if (pokemonList[i].height > 0.6) {
+        pokemonList[i].height = pokemonList[i].height + "-Wow, that's big!"
+    };
+    // Output Pokemon list to web page as block elements
+    document.write('<p>' + pokemonList[i].name + " " + 'height: ' + pokemonList[i].height + '</p>');
+}
