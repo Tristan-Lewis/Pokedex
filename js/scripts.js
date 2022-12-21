a505b92b2c9948340e5f7fcf6e7e3aca551068b0
Tristan Lewis <94727522+MajestysFiend@users.noreply.github.com>
2022-12-21 01:47:42 -0500

    Added IIFE with basic functionality and removed for loop in place of forEach method
    moved pokemon list into IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
-// Array of Pokemon and their attributes as objects
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['grass', 'poison'] },
-    { name: 'Charmander', height: 0.6, weight: 8.5, types: ['fire'] },
-    { name: 'Squirtle', height: 0.5, weight: 9, types: ['water'] }];
+//IIFE containing pokemon list and basic functionality
+let pokemonRepository = (function () {
+    // Array of Pokemon and their attributes as objects
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['grass', 'poison'] },
+        { name: 'Charmander', height: 0.6, weight: 8.5, types: ['fire'] },
+        { name: 'Squirtle', height: 0.5, weight: 9, types: ['water'] }];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    }
+
+})();
 // Looping through Pokemon list array
-for (i = 0; i < pokemonList.length; i++) {
+pokemonRepository.getAll().forEach(function (pokemon) {
     // Conditional for adding text to Pokemon height
-    if (pokemonList[i].height > 0.6) {
-        pokemonList[i].height = pokemonList[i].height + "-Wow, that's big!"
-    };
+    if (pokemon.height > 0.6) {
+        pokemon.height = pokemon.height + "-Wow, that's big!";
+    }
     // Output Pokemon list to web page as block elements
-    document.write('<p>' + pokemonList[i].name + " " + 'height: ' + pokemonList[i].height + '</p>');
-}
+    document.write('<p>' + pokemon.name + " " + 'height: ' + pokemon.height + '</p>');
+});
